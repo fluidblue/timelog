941d3a6f8fbf4d43441a0470b0d62af46e00e58d
fluidblue <fluidblue@users.noreply.github.com>
2022-02-01 18:45:43 +0100

    Added binding of date variable

--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -7,11 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainViewComponent implements OnInit {
 
-  daycardDate = new Date(2022, 0, 27);
+  dateFrom?: Date
+  dateTo?: Date
+
+  daycardDate = this.getBeginOfWeek();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getBeginOfWeek(): Date {
+    // TODO
+    return new Date(2022, 0, 27);
+  }
+
+  parseDateEvent(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    return this.parseDate(value);
+  }
+
+  parseDate(dateString?: string): Date | undefined {
+    if (!dateString) {
+      return undefined;
+    }
+    return new Date(dateString);
   }
 
 }
