0af5d8696e50a5a46e022840a3d9c591d6185073
fluidblue <fluidblue@users.noreply.github.com>
2022-02-02 15:50:35 +0100

    Calculate begin of week

--- src/app/main-view/main-view.component.spec.ts
+++ src/app/main-view/main-view.component.spec.ts
@@ -22,4 +22,34 @@ describe('MainViewComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should calculate the first day of the week', () => {
+    let startOfWeek = 1 // Monday
+    expect(component.getBeginOfWeek(new Date("2022-01-31"), startOfWeek)).toEqual(new Date("2022-01-31"));
+    expect(component.getBeginOfWeek(new Date("2022-02-01"), startOfWeek)).toEqual(new Date("2022-01-31"));
+    expect(component.getBeginOfWeek(new Date("2022-02-02"), startOfWeek)).toEqual(new Date("2022-01-31"));
+    expect(component.getBeginOfWeek(new Date("2022-02-03"), startOfWeek)).toEqual(new Date("2022-01-31"));
+    expect(component.getBeginOfWeek(new Date("2022-02-04"), startOfWeek)).toEqual(new Date("2022-01-31"));
+    expect(component.getBeginOfWeek(new Date("2022-02-05"), startOfWeek)).toEqual(new Date("2022-01-31"));
+    expect(component.getBeginOfWeek(new Date("2022-02-06"), startOfWeek)).toEqual(new Date("2022-01-31"));
+
+    startOfWeek = 0 // Sunday
+    expect(component.getBeginOfWeek(new Date("2022-01-31"), startOfWeek)).toEqual(new Date("2022-01-30"));
+    expect(component.getBeginOfWeek(new Date("2022-02-01"), startOfWeek)).toEqual(new Date("2022-01-30"));
+    expect(component.getBeginOfWeek(new Date("2022-02-02"), startOfWeek)).toEqual(new Date("2022-01-30"));
+    expect(component.getBeginOfWeek(new Date("2022-02-03"), startOfWeek)).toEqual(new Date("2022-01-30"));
+    expect(component.getBeginOfWeek(new Date("2022-02-04"), startOfWeek)).toEqual(new Date("2022-01-30"));
+    expect(component.getBeginOfWeek(new Date("2022-02-05"), startOfWeek)).toEqual(new Date("2022-01-30"));
+    expect(component.getBeginOfWeek(new Date("2022-02-06"), startOfWeek)).toEqual(new Date("2022-02-06"));
+
+    startOfWeek = 3 // Wednesday
+    expect(component.getBeginOfWeek(new Date("2022-01-31"), startOfWeek)).toEqual(new Date("2022-01-26"));
+    expect(component.getBeginOfWeek(new Date("2022-02-01"), startOfWeek)).toEqual(new Date("2022-01-26"));
+    expect(component.getBeginOfWeek(new Date("2022-02-02"), startOfWeek)).toEqual(new Date("2022-02-02"));
+    expect(component.getBeginOfWeek(new Date("2022-02-03"), startOfWeek)).toEqual(new Date("2022-02-02"));
+    expect(component.getBeginOfWeek(new Date("2022-02-04"), startOfWeek)).toEqual(new Date("2022-02-02"));
+    expect(component.getBeginOfWeek(new Date("2022-02-05"), startOfWeek)).toEqual(new Date("2022-02-02"));
+    expect(component.getBeginOfWeek(new Date("2022-02-06"), startOfWeek)).toEqual(new Date("2022-02-02"));
+
+  });
 });
--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DEFAULT_SETTINGS, Settings } from '../Settings';
+import { SettingsService } from '../settings.service';
+import { WEEK_DAYS } from '../WeekDays';
 
 @Component({
   selector: 'app-main-view',
@@ -7,19 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainViewComponent implements OnInit {
 
-  dateFrom?: Date = this.getBeginOfWeek()
-  dateTo?: Date = new Date()
+  settings: Settings = DEFAULT_SETTINGS;
+
+  dateFrom?: Date;
+  dateTo?: Date;
 
   daycardDate = new Date(2022, 0, 27); // TODO: Remove
 
-  constructor() { }
+  constructor(private settingsService: SettingsService) { }
 
   ngOnInit(): void {
+    this.getSettings();
   }
 
-  getBeginOfWeek(): Date {
-    // TODO: Implement
-    return new Date(2022, 0, 27);
+  updateView(): void {
+    this.dateFrom = this.getBeginOfWeek();
+    this.dateTo = new Date();
+  }
+
+  getSettings(): void {
+    this.settingsService.getSettings().subscribe((settings) => {
+      this.settings = settings;
+      this.updateView();
+    });
+  }
+
+  /**
+   * Returns a number (0 to 6) corresponding to the day (Sun, Mon, Tue, ..., Sat)
+   * at which the week starts.
+   */
+  getStartOfWeek(): number {
+    return WEEK_DAYS.indexOf(this.settings.startOfWeek);
+  }
+
+  /**
+   * Returns a `Date` corresponding to the first day of a week.
+   * 
+   * Posted to https://stackoverflow.com/a/70957586/2013757
+   * 
+   * @param startOfWeek Sets an arbitrary start of week (e.g. Sunday = 0, Monday = 1, Tuesday = 2, etc.)
+   */
+  getBeginOfWeek(date = new Date(), startOfWeek = this.getStartOfWeek()): Date {
+    const result = new Date(date);
+    while (result.getDay() !== startOfWeek) {
+      result.setDate(result.getDate() - 1);
+    }
+    return result;
   }
 
   parseDateEvent(event: Event) {
