357df083dac824b002eb56abd21b2f1b2447b3dd
fluidblue <fluidblue@users.noreply.github.com>
2022-02-03 10:15:31 +0100

    Using ng-bootstrap ngbCollapse

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  public isMenuCollapsed = true;
+
   constructor() { }
 
   ngOnInit(): void {
