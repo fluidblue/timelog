52660a80deb3ff21ef7465b62cb8b2c54f712a79
fluidblue <fluidblue@users.noreply.github.com>
2022-02-07 17:47:21 +0100

    Added fromString

--- src/app/Time.ts
+++ src/app/Time.ts
@@ -1,8 +1,35 @@
 export class Time {
     private minutesTotal: number;
 
+    private static reTime = /^(?<hours>[0-9]{1,2}):(?<minutes>[0-9]{2})$/gm;
+
     constructor(hours: number, minutes: number) {
         this.minutesTotal = minutes + hours * 60;
+    }
+
+    public static fromString(time: string): Time | null {
+        let negativeTime = false;
+        if (time.startsWith("-")) {
+            time = time.substring(1);
+            negativeTime = true;
+        }
+
+        const matchResult = time.match(Time.reTime);
+        if (!matchResult) {
+            return null;
+        }
+        const hours: number = parseInt(matchResult.groups!["hours"]);
+        const minutes: number = parseInt(matchResult.groups!["minutes"]);
+        if (hours < 0 || hours > 24 || minutes < 0 || minutes > 60) {
+            return null;
+        }
+        const timeObject = new Time(hours, minutes);
+
+        if (negativeTime) {
+            timeObject.multiply(-1);
+        }
+
+        return timeObject;
     }
 
     public substract(time: Time): Time {
