7fee444dbb3eeea782928f05a3ba8c877130c1c7
fluidblue <fluidblue@users.noreply.github.com>
2022-03-08 18:30:56 +0100

    Finished test

--- backend/spec/DatabaseSpec.ts
+++ backend/spec/DatabaseSpec.ts
@@ -13,7 +13,13 @@ describe("Database", () => {
 	it("should get and put the settings", async () => {
 		const settingsData: SettingsData = await database.settingsGet();
 
-		// TODO
-		console.log(settingsData);
+		expect(settingsData.weekStartsOn).toBeDefined();
+		expect(settingsData.workingTimes["monday"]).toBeDefined();
+		expect(settingsData.workingTimes["tuesday"]).toBeDefined();
+		expect(settingsData.workingTimes["wednesday"]).toBeDefined();
+		expect(settingsData.workingTimes["thursday"]).toBeDefined();
+		expect(settingsData.workingTimes["friday"]).toBeDefined();
+		expect(settingsData.workingTimes["saturday"]).toBeDefined();
+		expect(settingsData.workingTimes["sunday"]).toBeDefined();
 	});
 });
