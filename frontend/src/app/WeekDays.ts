b2acf0c67b657db11609553f0b67569b4e8f22fe
fluidblue <fluidblue@users.noreply.github.com>
2022-03-09 09:30:51 +0100

    WEEK_DAYS lowercase

--- frontend/src/app/WeekDays.ts
+++ frontend/src/app/WeekDays.ts
@@ -2,11 +2,11 @@
  * Days of the week, in the same order that `Date.prototype.getDay()` uses.
  */
 export const WEEK_DAYS = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday"
 ];
