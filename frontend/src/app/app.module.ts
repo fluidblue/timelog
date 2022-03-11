778b6d46708fa0d935f7564660a74a5b5ef336d6
fluidblue <fluidblue@users.noreply.github.com>
2022-03-11 12:51:00 +0100

    Added and implemented toast component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { SettingsComponent } from './settings/settings.component';
 import { UnderOverTimeComponent } from './under-over-time/under-over-time.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ToastsComponent } from './toasts/toasts.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HttpClientModule } from '@angular/common/http';
     MainViewComponent,
     NavbarComponent,
     SettingsComponent,
-    UnderOverTimeComponent
+    UnderOverTimeComponent,
+    ToastsComponent
   ],
   imports: [
     BrowserModule,
