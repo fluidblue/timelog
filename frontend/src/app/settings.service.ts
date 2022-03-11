ccd8329bdaf12b6194e8fc5f09c072df31eeb315
fluidblue <fluidblue@users.noreply.github.com>
2022-03-11 18:43:04 +0100

    Caching settings

--- frontend/src/app/settings.service.ts
+++ frontend/src/app/settings.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable, of } from 'rxjs';
+import { map, Observable, of, tap } from 'rxjs';
 import { Settings, SettingsJson, WeekDay } from './Settings';
 import API from './API';
 import { Time } from './Time';
@@ -11,7 +11,9 @@ import { ResponseJson } from './ResponseJson';
 })
 export class SettingsService {
 
-  private apiUri = API.apiUri + "/settings"
+  private readonly apiUri = API.apiUri + "/settings"
+
+  private settings?: Settings;
 
   constructor(
     private http: HttpClient
@@ -48,11 +50,21 @@ export class SettingsService {
   }
 
   getSettings(): Observable<Settings> {
+    if (this.settings) {
+      return of(this.settings);
+    }
+
     const observable = this.http.get<SettingsJson>(this.apiUri);
-    return observable.pipe(map((settingsJson) => this.convertJsonToSettings(settingsJson)));
+    return observable.pipe(
+      map((settingsJson) => this.convertJsonToSettings(settingsJson))
+    ).pipe(
+      tap((settings) => this.settings = settings)
+    );
   }
 
   setSettings(settings: Settings): Observable<ResponseJson> {
+    this.settings = settings;
+
     const settingsJson = this.convertSettingsToJson(settings);
     return this.http.put<ResponseJson>(this.apiUri, settingsJson);
   }
