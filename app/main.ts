09fb76b0e5dbafa1f209379cf9f7423e74187362
fluidblue <fluidblue@users.noreply.github.com>
2022-03-17 14:01:12 +0100

    Cleanup

--- app/main.ts
+++ app/main.ts
@@ -2,7 +2,7 @@ import { app, BrowserWindow } from "electron";
 import * as path from "path";
 
 function createWindow() {
-  // Create the browser window.
+  // Create the browser window
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -11,7 +11,6 @@ function createWindow() {
     },
   });
 
-  console.log("app.isPackaged", app.isPackaged); // TODO: Remove
   if (app.isPackaged) {
     // Load the index.html of the app
     mainWindow.loadFile(path.join(__dirname, "./timelog/index.html"));
@@ -19,7 +18,7 @@ function createWindow() {
     // Hot reload in dev mode
     mainWindow.loadURL("http://localhost:4200");
 
-    // Open the DevTools.
+    // Open the DevTools
     mainWindow.webContents.openDevTools();
   }
 }
@@ -33,18 +32,17 @@ app.on("ready", () => {
   app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed, except on macOS.
+// On macOS, it's common for applications and their menu bar to stay active until
+// the user quits explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
--- app/preload.ts
+++ app/preload.ts
@@ -1,7 +1,2 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener("DOMContentLoaded", () => {
-  for (const type of ["chrome", "node", "electron"]) {
-    console.log(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions]);
-  }
-});
