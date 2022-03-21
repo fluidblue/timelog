47af901ba9affd9345051e2ada4025af7edd278d
fluidblue <fluidblue@users.noreply.github.com>
2022-03-21 22:54:55 +0100

    Migrated DB from MariaDB to SQLite

--- electron/db.sql
+++ electron/db.sql
@@ -1,45 +1,33 @@
--- Adminer 4.8.1 MySQL 5.5.5-10.7.1-MariaDB-1:10.7.1+maria~focal dump
+-- Adminer 4.8.1 SQLite 3 3.36.0 dump
 
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+DROP TABLE IF EXISTS "Settings";
+CREATE TABLE "Settings" (
+  "weekStartsOn" text NOT NULL
+);
 
-SET NAMES utf8mb4;
+INSERT INTO "Settings" ("weekStartsOn") VALUES ('monday');
 
-DROP TABLE IF EXISTS `Settings`;
-CREATE TABLE `Settings` (
-  `weekStartsOn` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO `Settings` (`weekStartsOn`) VALUES
-('monday');
-
-DROP TABLE IF EXISTS `TimeLog`;
-CREATE TABLE `TimeLog` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date` date NOT NULL,
-  `from` time NOT NULL,
-  `to` time NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS "TimeLog";
+CREATE TABLE "TimeLog" (
+  "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "date" integer NOT NULL,
+  "from" integer NOT NULL,
+  "to" integer NOT NULL
+);
 
 
-DROP TABLE IF EXISTS `WorkingTimes`;
-CREATE TABLE `WorkingTimes` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `workingTime` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS "WorkingTimes";
+CREATE TABLE "WorkingTimes" (
+  "id" integer NOT NULL,
+  "name" text NOT NULL,
+  "workingTime" integer NOT NULL,
+  PRIMARY KEY ("id")
+);
 
-INSERT INTO `WorkingTimes` (`id`, `name`, `workingTime`) VALUES
-(0,	'sunday',	0),
-(1,	'monday',	480),
-(2,	'tuesday',	480),
-(3,	'wednesday',	480),
-(4,	'thursday',	480),
-(5,	'friday',	480),
-(6,	'saturday',	0);
-
--- 2022-03-08 17:44:01
+INSERT INTO "WorkingTimes" ("id", "name", "workingTime") VALUES (0,	'sunday',	0);
+INSERT INTO "WorkingTimes" ("id", "name", "workingTime") VALUES (1,	'monday',	480);
+INSERT INTO "WorkingTimes" ("id", "name", "workingTime") VALUES (2,	'tuesday',	480);
+INSERT INTO "WorkingTimes" ("id", "name", "workingTime") VALUES (3,	'wednesday',	480);
+INSERT INTO "WorkingTimes" ("id", "name", "workingTime") VALUES (4,	'thursday',	480);
+INSERT INTO "WorkingTimes" ("id", "name", "workingTime") VALUES (5,	'friday',	480);
+INSERT INTO "WorkingTimes" ("id", "name", "workingTime") VALUES (6,	'saturday',	0);
